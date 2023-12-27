d61bc964584b4f149b77579dfb91bd98ae933062
adamnoack <adamnoack1@gmail.com>
2023-12-27 11:16:05 -0800

    Make plot interaactive and much better

--- app.py
+++ app.py
@@ -1,22 +1,23 @@
+import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
+import plotly.express as px
 import streamlit as st
+
 
 
 # ========== Load the data ==========
 filename = "lab_results/Lab results master sheet.xlsx"
 dfs = pd.read_excel(filename, sheet_name=None)
-df = dfs['Sheet1']  # get "Sheet1" (there's only one sheet for now)
-df.head()
+df_orig = dfs['Sheet1']  # get "Sheet1" (there's only one sheet for now)
 
 
 # ========== Get data into correct format ==========
-columns = df.columns
+columns = df_orig.columns
 test_datetimes = columns[5:]
 test_dates = [d.split(':')[0] for d in test_datetimes]
 
 data = {"Date": test_dates}
-for i, row in df.iterrows():
+for i, row in df_orig.iterrows():
     test_name = row['Test Name']
     values = []
     for test_datetime in test_datetimes:
@@ -32,13 +33,35 @@ st.title("Blood Marker Analysis")
 
 # Dropdown menu for selecting a blood marker
 option = st.selectbox("Select a Blood Marker", df.columns[1:])
+ref_range_lower = df_orig.loc[df_orig['Test Name'] == option, 'Ref. Range Lower'].values[0]
+ref_range_upper = df_orig.loc[df_orig['Test Name'] == option, 'Ref. Range Upper'].values[0]
 
-# Plotting
-fig, ax = plt.subplots()
-ax.plot(df['Date'], df[option], marker='o', linestyle='-')
-ax.set_title(f"{option} over Time")
-ax.set_xlabel("Date")
-ax.set_ylabel(option)
-ax.grid(True)
+# Plotly plot
+fig = px.line(df, x='Date', y=option, markers=True, title=f"{option} over Time")
+fig.update_traces(mode='markers+lines', hoverinfo='text+name', text=df[option])
+fig.update_layout(hovermode='x unified')
 
-st.pyplot(fig)
+# make sure max and min values are covered in range
+readings = [r for r in list(df[option]) if not np.isnan(r)]
+min_y = min(min(readings), ref_range_lower)
+max_y = max(max(readings), ref_range_upper)
+
+# Adding horizontal lines for reference values
+if not np.isnan(ref_range_lower):
+    fig.add_hline(y=ref_range_lower, line_dash="dash", line_color="red")
+if not np.isnan(ref_range_upper):
+    fig.add_hrect(y0=ref_range_lower, y1=ref_range_upper, line_width=0, fillcolor="green", opacity=0.2)
+    fig.add_hline(y=ref_range_upper, line_dash="dash", line_color="red")
+
+# Adjusting y-axis limits
+buffer = 0.4 * (max_y - min_y)  # 10% buffer above and below the reference range
+fig.update_yaxes(range=[min_y - buffer, max_y + buffer])
+
+# Calculate differences between consecutive values
+percent_diffs = [(readings[i+1] - readings[i]) / readings[i] for i in range(len(readings) - 1)]
+percent_diffs = [p*100 for p in percent_diffs]
+
+for i in range(len(percent_diffs)):
+    st.text(f"Percent difference between readings {i} and {i+1}: {percent_diffs[i]:.2f}%")
+
+st.plotly_chart(fig)
